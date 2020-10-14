886758bf61959ce8ad7b56c00f190e59d5a0f375
Jamie Stevens <ste616@gmail.com>
2020-10-14 12:27:51 +1100

    I've decided to use Doxygen to document the library, and the effort has begun here.

--- src/rpfits/atrpfits.h
+++ src/rpfits/atrpfits.h
@@ -1,16 +1,12 @@
-/**
- * ATCA Training Library: reader.h
- * (C) Jamie Stevens CSIRO 2019
+/** \file atrpfits.h
+ *  \brief Definitions for low-level RPFITS-related parameters.
  *
- * This library is designed to read an RPFITS file and for each cycle make available
- * the products which would be available online: spectra, amp, phase, delay,
- * u, v, w, etc.
- * This is so we can make tools which will let observers muck around with what they 
- * would see online in all sorts of real situations, as represented by real data 
- * files.
- * 
- * This module contains the structures and common-use functions necessary for all 
- * the other routines.
+ * ATCA Training Library: atrpfits.h
+ * (C) Jamie Stevens CSIRO 2020
+ *
+ * This file contains the structures and common-use functions necessary for all 
+ * the other routines, along with some magic numbers.
+ *
  */
 
 #pragma once
@@ -18,28 +14,107 @@
 #include <complex.h>
 
 // Some check values.
+/*! \def RPFITS_FLAG_BAD
+ *  \brief A magic number for bad data. **UNUSED**
+ *
+ *  This magic number is currently unused.
+ */
 #define RPFITS_FLAG_BAD 1
+/*! \def RPFITS_FLAG_GOOD
+ *  \brief A magic number for good data. **UNUSED**
+ *
+ *  This magic number is currently unused.
+ */
 #define RPFITS_FLAG_GOOD 0
 
+/*! \def OBSDATE_LENGTH
+ *  \brief The length of an RPFITS date string.
+ *
+ *  This length is set in the RPFITS definition of `nx_date` to be 12.
+ *
+ *  All RPFITS date strings are formatted like 2020-11-20
+ *  (10 characters).
+ */
 #define OBSDATE_LENGTH 12
+/*! \def OBSTYPE_LENGTH
+ *  \brief The length of an RPFITS observation type string.
+ *
+ * This length is set in the RPFITS definition of `obstype` to be 16.
+ *
+ * Examples of RPFITS observation types are:
+ * - DWELL
+ * - POINT
+ */
 #define OBSTYPE_LENGTH 16
+/*! \def SOURCE_LENGTH
+ *  \brief The length of an RPFITS source name string.
+ *
+ * This length is set in the RPFITS definition of `nx_source` to be 16.
+ */
 #define SOURCE_LENGTH 16
+/*! \def CALCODE_LENGTH
+ *  \brief The length of an RPFITS calibrator code string.
+ *
+ * This length is set in the RPFITS definition of `su_cal` to be 4.
+ *
+ * Examples of RPFITS calibrator codes are:
+ * - C (usually for a known calibrator)
+ * - B (observed during a baseline solution)
+ */
 #define CALCODE_LENGTH 4
 
-// The largest number of baselines we support.
-// This is for up to 100 antennas.
+/*! \def MAX_BASELINENUM
+ *  \brief The largest number of baselines we support.
+ *
+ * This parameter sets the length of the all_baselines array in the
+ * cycle_data structure. The baseline number is determined with the
+ * ants_to_base routine. This number allows for up to 100 antennas,
+ * which is not really necessary for ATCA use, but will mean it doesn't
+ * need altering should this library be used for larger arrays.
+ *
+ * We use this array in this way so we can quickly and efficiently
+ * identify which baseline pairs are available in the data (by marking
+ * the array entry according to the unique baseline number).
+ */
 #define MAX_BASELINENUM 25700
 
-#define FILENAME_LENGTH 2048
+/*! \def FILENAME_LENGTH
+ *  \brief The maximum length of a filename we can use.
+ *
+ * This length is set in the RPFITS definition of `file` to be 256.
+ */
+#define FILENAME_LENGTH 256
 
-/**
- * This structure holds all the header information data.
+/*! \struct scan_header_data
+ *  \brief This structure holds all the header information data.
+ *
+ * When reading from the RPFITS file, each scan has a header which
+ * describes the metadata for that scan. The reader routine takes
+ * the header data and stores it in this structure for easy access.
  */
 struct scan_header_data {
   // Time variables.
+  /*! \var obsdate
+   *  \brief The base date of the scan.
+   *
+   * The base date of the scan will be the date when the file
+   * containing the scan was opened. It is therefore possible
+   * that this variable does not represent the actual date of
+   * the scan, but this can be determined with the ut_seconds
+   * variable. All dates have the format YYYY-MM-DD, where MM
+   * and DD start at 01, and are in UTC.
+   */
   char obsdate[OBSDATE_LENGTH];
+  /*! \var ut_seconds
+   *  \brief The number of seconds after the base date that this
+   *         scan was started.
+   *
+   * This scan started this number of seconds after UTC midnight
+   * on the base date represented by the obsdate variable.
+   */
   float ut_seconds;
   // Details about the observation.
+  
   char obstype[OBSTYPE_LENGTH];
   char calcode[CALCODE_LENGTH];
   int cycle_time;
