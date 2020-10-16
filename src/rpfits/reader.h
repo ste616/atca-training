cef4330a6d27909e27d6b3bb6cbdba7e4e6f3d83
Jamie Stevens <ste616@gmail.com>
2020-10-16 15:40:42 +1100

    Lots more documentation

--- src/rpfits/reader.h
+++ src/rpfits/reader.h
@@ -99,41 +99,289 @@
 /* Macros to get information. */
 /*! \def SOURCENAME
  *  \brief Get the name of source \a s from the RPFITS variables.
- *  \param s the number of the source
- *  \returns a pointer to the start of the source name string
+ *  \param s the number of the source (starts at 0)
+ *  \return a pointer to the start of the source name string
  *
  * A convenience macro to determine the pointer location to obtain the
  * name of the source with number \a s from the RPFITS variables, after
  * reading in the header.
  */
 #define SOURCENAME(s) names_.su_name + (s) * SOURCE_LENGTH
+/*! \def RIGHTASCENSION
+ *  \brief Get the right ascension of source \a s from the RPFITS variables.
+ *  \param s the number of the source (starts at 0)
+ *  \return right ascension of the source in radians [double]
+ *
+ * A convenience macro to retrieve the Right Ascension of source with
+ * number \a s from the RPFITS variables, after reading in the header.
+ */
 #define RIGHTASCENSION(s) doubles_.su_ra[s]
+/*! \def DECLINATION
+ *  \brief Get the right ascension of source \a s from the RPFITS variables.
+ *  \param s the number of the source (starts at 0)
+ *  \return right ascension of the source in radians [double]
+ *
+ * A convenience macro to retrieve the Declination of source with
+ * number \a s from the RPFITS variables, after reading in the header.
+ */
 #define DECLINATION(s) doubles_.su_dec[s]
+/*! \def CALCODE
+ *  \brief Get the calibrator code of source \a s from the RPFITS variables.
+ *  \param s the number of the source (starts at 0)
+ *  \return a pointer to the start of the calibrator code string
+ *
+ * A convenience macro to determine the pointer location to obtain the
+ * calibrator code of the source with number \a s from the RPFITS variables,
+ * after reading in the header.
+ */
 #define CALCODE(s) names_.su_cal + (s) * CALCODE_LENGTH
+/*! \def FREQUENCYMHZ
+ *  \brief Get the central frequency of IF \a f from the RPFITS variables.
+ *  \param f the number of the IF (starts at 0)
+ *  \return central frequency of the IF in MHz [double]
+ *
+ * A convenience macro to retrieve the frequency at the centre of the IF with
+ * number \a f from the RPFITS variables, after reading in the header.
+ */
 #define FREQUENCYMHZ(f) doubles_.if_freq[f] / 1e6
+/*! \def BANDWIDTHMHZ
+ *  \brief Get the total bandwidth of IF \a f from the RPFITS variables.
+ *  \param f the number of the IF (starts at 0)
+ *  \return total bandwidth of the IF in MHz [double]
+ *
+ * A convenience macro to retrieve the total bandwidth of the IF with
+ * number \a f from the RPFITS variables, after reading in the header.
+ */
 #define BANDWIDTHMHZ(f) doubles_.if_bw[f] / 1e6
+/*! \def NCHANNELS
+ *  \brief Get the number of channels in IF \a f from the RPFITS variables.
+ *  \param f the number of the IF (starts at 0)
+ *  \return number of channels in the IF [int]
+ *
+ * A convenience macro to retrieve the number of channels in the IF with
+ * number \a f from the RPFITS variables, after reading in the header.
+ */
 #define NCHANNELS(f) if_.if_nfreq[f]
+/*! \def NSTOKES
+ *  \brief Get the number of Stokes parameters in IF \a f from the RPFITS variables.
+ *  \param f the number of the IF (starts at 0)
+ *  \return number of Stokes parameters in the IF [int]
+ *
+ * A convenience macro to retrieve the number of Stokes parameters in the IF with
+ * number \a f from the RPFITS variables, after reading in the header.
+ */
 #define NSTOKES(f) if_.if_nstok[f]
+/*! \def SIDEBAND
+ *  \brief Get a flag representing the sideband of IF \a f from the RPFITS variables.
+ *  \param f the number of the IF (starts at 0)
+ *  \return either +1 for USB IF, or -1 for LSB IF [int]
+ *
+ * A convenience macro to retrieve the sideband inversion factor for the IF with
+ * number \a f from the RPFITS variables, after reading in the header.
+ */
 #define SIDEBAND(f) if_.if_invert[f]
+/*! \def CHAIN
+ *  \brief Get the number of the RF signal chain used to produce the IF \a f
+ *         from the RPFITS variables
+ *  \param f the number of the IF (starts at 0)
+ *  \return the RF signal chain number, which starts at 1 [int]
+ *
+ * A convenience macro to retrieve the RF signal chain number used to produce the
+ * IF with number \a f from the RPFITS variables, after reading in the header.
+ */
 #define CHAIN(f) if_.if_chain[f]
+/*! \def LABEL
+ *  \brief Get the assigned number of the IF \a f from the RPFITS variables
+ *  \param f the number of the IF (starts at 0)
+ *  \return the assigned IF number, which starts at 1 [int]
+ *
+ * A convenience macro to retrieve the assigned IF number for the
+ * IF with number \a f from the RPFITS variables, after reading in the header.
+ */
 #define LABEL(f) if_.if_num[f]
+/*! \def CSTOKES
+ *  \brief Get the name for the \a s Stokes parameter in IF \a f from the
+ *         RPFITS variables
+ *  \param f the number of the IF (starts at 0)
+ *  \param s the number of the Stokes parameter (starts at 0)
+ *  \return a pointer to the start of the Stokes parameter string
+ *
+ * A convenience macro to determine the pointer location to obtain the
+ * Stokes parameter name for the parameter with number \a s in the IF with
+ * number \a f from the RPFITS variables, after reading in the header.
+ */
 #define CSTOKES(f,s) names_.if_cstok + (s * 2 + f * 4 * 2)
+/*! \def ANTNUM
+ *  \brief Get the assigned number of the antenna \a a from the RPFITS variables
+ *  \param a the number of the antenna (starts at 0)
+ *  \return the assigned number of the antenna, which starts at 1 [int]
+ *
+ * A convenience macro to retrieve the assigned antenna number for the antenna
+ * with number \a a from the RPFITS variables, after reading in the header.
+ */
 #define ANTNUM(a) anten_.ant_num[a]
+/*! \def ANTSTATION
+ *  \brief Get the name of the station that antenna \a a is connected to from
+ *         the RPFITS variables
+ *  \param a the number of the antenna (starts at 0)
+ *  \return a pointer to the start of the station name string
+ *
+ * A convenience macro to determine the pointer location to obtain the station
+ * name that the antenna with number \a a is connected to from the RPFITS variables,
+ * after reading in the header.
+ */
 #define ANTSTATION(a) names_.sta + (a * 8)
+/*! \def ANTX
+ *  \brief Get the X-coordinate of the antenna \a a from the RPFITS variables
+ *  \param a the number of the antenna (starts at 0)
+ *  \return the X-coordinate of the position of antenna \a a on the WGS84
+ *          Cartesian plane, in m [double]
+ *
+ * A convenience macro to retrieve the position of the antenna with number \a a
+ * from the RPFITS variables, after reading in the header. To get the full position
+ * this macro needs to be used along with two others.
+ */
 #define ANTX(a) doubles_.x[a]
+/*! \def ANTY
+ *  \brief Get the Y-coordinate of the antenna \a a from the RPFITS variables
+ *  \param a the number of the antenna (starts at 0)
+ *  \return the Y-coordinate of the position of antenna \a a on the WGS84
+ *          Cartesian plane, in m [double]
+ *
+ * A convenience macro to retrieve the position of the antenna with number \a a
+ * from the RPFITS variables, after reading in the header. To get the full position
+ * this macro needs to be used along with two others.
+ */
 #define ANTY(a) doubles_.y[a]
+/*! \def ANTZ
+ *  \brief Get the Z-coordinate of the antenna \a a from the RPFITS variables
+ *  \param a the number of the antenna (starts at 0)
+ *  \return the Z-coordinate of the position of antenna \a a on the WGS84
+ *          Cartesian plane, in m [double]
+ *
+ * A convenience macro to retrieve the position of the antenna with number \a a
+ * from the RPFITS variables, after reading in the header. To get the full position
+ * this macro needs to be used along with two others.
+ */
 #define ANTZ(a) doubles_.z[a]
 
 /* These macros allow for easier interrogation of the SYSCAL table. */
+/*! \def SYSCAL_NUM_ANTS
+ *  \brief Get the number of antennas represented in the SYSCAL table from the
+ *         RPFITS variables
+ *  \return the number of antennas in the SYSCAL table [int]
+ *
+ * A convenience macro to retrieve the number of antennas that the SYSCAL table
+ * contains information about. This will only return a non-zero number if the SYSCAL
+ * table has just been read. This macro is here only to make the code more readable.
+ */
 #define SYSCAL_NUM_ANTS   sc_.sc_ant
+/*! \def SYSCAL_NUM_IFS
+ *  \brief Get the number of IFs represented in the SYSCAL table from the
+ *         RPFITS variables
+ *  \return the number of IFs in the SYSCAL table [int]
+ *
+ * A convenience macro to retrieve the number of IFs that the SYSCAL table
+ * contains information about. This will only return a non-zero number if the SYSCAL
+ * table has just been read. This macro is here only to make the code more readable.
+ */
 #define SYSCAL_NUM_IFS    sc_.sc_if
+/*! \def SYSCAL_NUM_PARAMS
+ *  \brief Get the number of parameters represented in the SYSCAL table from the
+ *         RPFITS variables
+ *  \return the number of parameters in the SYSCAL table [int]
+ *
+ * A convenience macro to retrieve the number of parameters that the SYSCAL table
+ * contains information about. This will only return a non-zero number if the SYSCAL
+ * table has just been read. This macro is here only to make the code more readable.
+ * A SYSCAL parameter is something like Tsys, or parallactic angle etc.
+ */
 #define SYSCAL_NUM_PARAMS sc_.sc_q
+/*! \def SYSCAL_GRP
+ *  \brief Get the array index for the SYSCAL parameters pertaining to antenna
+ *         \a a and IF \a i
+ *  \param a the number of the antenna (starts at 0)
+ *  \param i the number of the IF (starts at 0)
+ *  \return the array index at the start of the SYSCAL record for antenna \a a
+ *          and IF \a i
+ *
+ * Convenience macro to index the SYSCAL array from the RPFITS variables. Since
+ * the SYSCAL array is a single flat array, the index depends on how many IFs are
+ * present, how many parameters are presented, and which antenna and IF is desired.
+ * This macro is here only to make the code more readable.
+ */
 #define SYSCAL_GRP(a, i) (a * (SYSCAL_NUM_IFS * SYSCAL_NUM_PARAMS) + i * SYSCAL_NUM_PARAMS)
+/*! \def SYSCAL_PARAM
+ *  \brief Get the parameter value for the SYCAL parameter with index \a o pertaining 
+ *         to antenna \a a and IF \a i
+ *  \param a the number of the antenna (starts at 0)
+ *  \param i the number of the IF (starts at 0)
+ *  \param o the number of the parameter (starts at 0)
+ *  \return the value of SYSCAL parameter with index \a o for antenna \a a and IF \a i [float]
+ *  
+ * Convenience macro to return SYSCAL parameter values from the RPFITS variables.
+ * This macro is here only to make the code a lot more readable.
+ */
 #define SYSCAL_PARAM(a, i, o) (sc_.sc_cal[SYSCAL_GRP(a, i) + o])
+/*! \def SYSCAL_ANT
+ *  \brief Get the assigned antenna number for the SYSCAL parameter pertaining to
+ *         antenna \a and IF \i
+ *  \param a the number of the antenna (starts at 0)
+ *  \param i the number of the IF (starts at 0)
+ *  \return the assigned antenna number (starts at 1) [int]
+ *
+ * Convenience macro to return the assigned antenna number for a particular SYSCAL entry.
+ */
 #define SYSCAL_ANT(a, i) (int)SYSCAL_PARAM(a, i, 0)
+/*! \def SYSCAL_IF
+ *  \brief Get the assigned IF number for the SYSCAL parameter pertaining to
+ *         antenna \a and IF \i
+ *  \param a the number of the antenna (starts at 0)
+ *  \param i the number of the IF (starts at 0)
+ *  \return the assigned IF number (starts at 1) [int]
+ *
+ * Convenience macro to return the assigned IF number for a particular SYSCAL entry.
+ * This macro will only return a valid value if the SYSCAL table has just been read.
+ */
 #define SYSCAL_IF(a, i) (int)SYSCAL_PARAM(a, i, 1)
+/*! \def SYSCAL_XYPHASE
+ *  \brief Get the phase between the X and Y polarisations from the SYSCAL parameter 
+ *         pertaining to antenna \a and IF \i
+ *  \param a the number of the antenna (starts at 0)
+ *  \param i the number of the IF (starts at 0)
+ *  \return the phase between the X and Y polarisations, as measured by the correlator,
+ *          for antenna \a a in IF \a i, in degrees [float]
+ *
+ * Convenience macro to return the correlator-measured XY-phase for a specified
+ * antenna \a a and IF \a i.
+ * This macro will only return a valid value if the SYSCAL table has just been read.
+ */
 #define SYSCAL_XYPHASE(a, i) SYSCAL_PARAM(a, i, 2)
+/*! \def SYSCAL_TSYS_X
+ *  \brief Get the system temperature of the X polarisation from the SYSCAL parameter 
+ *         pertaining to antenna \a and IF \i
+ *  \param a the number of the antenna (starts at 0)
+ *  \param i the number of the IF (starts at 0)
+ *  \return the system temperature of the X polarisation, as measured by the correlator,
+ *          for antenna \a a in IF \a i, in Kelvin [float]
+ *
+ * Convenience macro to return the correlator-measured X-pol receiver system temperature
+ * for a specified antenna \a a and IF \a i.
+ * This macro will only return a valid value if the SYSCAL table has just been read.
+ */
 #define SYSCAL_TSYS_X(a, i) fabsf(SYSCAL_PARAM(a, i, 3))
+/*! \def SYSCAL_TSYS_Y
+ *  \brief Get the system temperature of the Y polarisation from the SYSCAL parameter 
+ *         pertaining to antenna \a and IF \i
+ *  \param a the number of the antenna (starts at 0)
+ *  \param i the number of the IF (starts at 0)
+ *  \return the system temperature of the Y polarisation, as measured by the correlator,
+ *          for antenna \a a in IF \a i, in Kelvin [float]
+ *
+ * Convenience macro to return the correlator-measured Y-pol receiver system temperature
+ * for a specified antenna \a a and IF \a i.
+ * This macro will only return a valid value if the SYSCAL table has just been read.
+ */
 #define SYSCAL_TSYS_Y(a, i) fabsf(SYSCAL_PARAM(a, i, 4))
 #define SYSCAL_TSYS_X_APPLIED(a, i) (SYSCAL_PARAM(a, i, 3) < 0) ? 0 : 1
 #define SYSCAL_TSYS_Y_APPLIED(a, i) (SYSCAL_PARAM(a, i, 4) < 0) ? 0 : 1
